ebcb31892cf391a179a7e9403646c9baf447a398
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-08-29 13:39:20 +0300

    add a new toggler for themes. old one was giving css issue with different browsers

--- src/components/ToggleTheme.js
+++ src/components/ToggleTheme.js
@@ -1,18 +1,13 @@
 import React from "react";
+import Toggle from 'react-toggle'
+import "react-toggle/style.css"
 
 const ToggleTheme = ({darkMode, setDarkMode}) => (
     <div className="toggle-container float-right">
         <span style={{color: darkMode ? "grey" : "yellow"}}><i className="sun fas fa-sun"></i></span>
-        <span className="toggle">
-            <input
-                checked={darkMode}
-                onChange={() => setDarkMode(prevMode => !prevMode)}
-                id="checkbox"
-                className="checkbox"
-                type="checkbox"
-            />
-            <label htmlFor="checkbox"/>
-            </span>
+        <label style={{marginTop: "9px"}} className="mx-1">
+            <Toggle defaultChecked={darkMode} className={`theme-toggle ${darkMode ? "dark-mode-true" : "dark-mode-false"}`} icons={false} onChange={()=>setDarkMode(prevMode => !prevMode)}/>
+        </label>
         <span style={{color: darkMode ? "slateblue" : "grey"}}><i className="moon fas fa-moon"></i></span>
     </div>
 );
